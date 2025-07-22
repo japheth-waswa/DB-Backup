2e188854596aaf400fc6e47b6af02c8f0ec56d5a
japheth-waswa <japhethwaswa@gmail.com>
2025-07-22 13:36:12 +0300

    fix(AppBackupApplicationTests)

--- src/test/java/com/zurimate/appbackup/AppBackupApplicationTests.java
+++ src/test/java/com/zurimate/appbackup/AppBackupApplicationTests.java
@@ -1,6 +1,5 @@
 package com.zurimate.appbackup;
 
-import com.zurimate.appbackup.dto.DBConfig;
 import com.zurimate.appbackup.service.BackupContext;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
@@ -17,7 +16,7 @@ class AppBackupApplicationTests {
     @Autowired
     private BackupContext backupContext;
 
-    private static final DBConfig mysqlDbConfig = TestDBConfigs.getMysqlConfig();
+//    private static final DBConfig mysqlDbConfig = TestDBConfigs.getMysqlConfig();
     private String mysqlDestinationRelative = "test-backup-rel";
     private String mysqlDestinationAbsolute = "/Users/japhethelijah/dev/java/apps/AppBackup/test-backup-abs";
     private List<String> mysqlOtherDirsToBackup = List.of("/Users/japhethelijah/Downloads/AppBackup", "/Users/japhethelijah/Downloads/smis");
@@ -28,7 +27,7 @@ class AppBackupApplicationTests {
 
     @Test
     void mysqlBackupTest_Relative_Destination() throws InterruptedException {
-       var res= backupContext.backup(mysqlDbConfig, mysqlDestinationRelative, mysqlOtherDirsToBackup);
+       var res= backupContext.backup(TestDBConfigs.getMysqlConfig(), mysqlDestinationRelative, mysqlOtherDirsToBackup);
        log.info("{}",res);
        Thread.sleep(15000);
     }
